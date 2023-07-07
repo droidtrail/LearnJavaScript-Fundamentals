8a76654367f104eab0bd44b8654fef0ea02a819b
Leandro Pereira <leandro.nares@gmail.com>
2023-07-07 18:08:17 -0300

    71. Implementing For and While Loop in JavaScript

--- basics2.js
+++ basics2.js
@@ -1,19 +1,36 @@
-const flag = true;
+// const flag = true;
 
-if (!flag) {
-    console.log("condition satisfied");
-}
-else {
-    console.log("condition NOT satisfied");
+// if (!flag) {
+//     console.log("condition satisfied");
+// }
+// else {
+//     console.log("condition NOT satisfied");
+// }
+
+// let i = 0
+// while(i>10){
+//     i++
+//     console.log("I am inside loop: " + i)
+// }
+
+// do{
+//     i++
+// }while(i>10);
+// console.log(i)
+let n = 0;
+for (let k = 1; k <= 100; k++) {
+    if (k % 2 == 0 && k % 5 == 0) {
+        n++
+        console.log("Resultado: " + k)
+        if (n == 3) {
+            break;
+        }
+    }
 }
 
-let i = 0
-while(i>10){
-    i++
-    console.log("I am inside loop: " + i)
-}
 
-do{
-    i++
-}while(i>10);
-console.log(i)
+// let required = true
+// while(required){
+//     console.log(required)
+//     required=false
+// }
