8405a852ff70816871c8b74d0d5f375069ff634a
Leandro Pereira <leandro.nares@gmail.com>
2023-07-09 20:45:16 -0300

    72. Arrays and its methods in JavaScript with Detailed examples - Part 1

---
+++ basics3.js
@@ -0,0 +1,14 @@
+let m1 = Array(6)
+var m2 = new Array (20,40,35,12,37,100)
+
+var m3 = [20,40,35,12,37,100]
+console.log('Index na posição 2: ' + m3[2]);
+m3[3]=14
+console.log('Valores de m3: ' + m3);
+console.log('Tamanho do vetor:' + m3.length);
+m3.push(65);
+console.log('Valores de m3: ' + m3);
+m3.pop();
+console.log('Valores de m3: ' + m3);
+m3.unshift(12);
+console.log('Valores de m3: ' + m3);
